55ab9e2e8e86321bbe6e8037de7670bca50144e2
minhuikim <kmh940@gmail.com>
2023-04-30 21:55:22 +0900

    TodoList UI 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-// import logo from './logo.svg';
-import './App.css';
-import Todo from './Todo';
+import "./App.css";
+import Todo from "./Todo";
+import React, {useState} from "react";
+import {List, Paper} from "@mui/material";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World 1",
+      done: true,
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true,
+    }
+  ]);
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{margin: 16}}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id}/>
+        ))}
+      </List>
+    </Paper>
+  );
+
   return (
     <div className="App">
-      <Todo />
-      <Todo />
+      {todoItems}
     </div>
   );
 }
