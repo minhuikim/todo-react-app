c5786f20537126313140b14bb752cf77d88209a6
minhuikim <kmh940@gmail.com>
2023-06-29 18:14:46 +0900

    Todo 삭제기능 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.css";
 import Todo from "./Todo";
-import React, {useState} from "react";
-import {Container, List, Paper} from "@mui/material";
+import React, { useState } from "react";
+import { Container, List, Paper } from "@mui/material";
 import AddTodo from "./AddTodo";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: "0",
-      title: "Hello World 1",
-      done: true,
-    },
-    {
-      id: "1",
-      title: "Hello World 2",
-      done: true,
-    }
-  ]);
+  // deleteItem 추가 후 테스트를 위해 작성한 useState([...]) 배열을 빈 배열로 초기화
+  const [items, setItems] = useState([]);
 
   const addItem = (item) => {
     item.id = "ID-" + item.length;  // key를 위한 id
@@ -24,14 +14,22 @@ function App() {
 
     // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 한다.
     setItems([...items, item]);     // [...items] : 새 배열 추가
-    console.log("items : ", items);
+    // console.log("items : ", items);
   };
+
+  const deleteItem = (item) => {
+    // 삭제할 아이템을 찾는다.
+    const newItems = items.filter(e => e.id != item.id);
+
+    // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장한다.
+    setItems([...newItems])
+  }
 
   let todoItems = items.length > 0 && (
     <Paper style={{margin: 16}}>
       <List>
         {items.map((item) => (
-          <Todo item={item} key={item.id}/>
+          <Todo item={item} key={item.id} deleteItem={deleteItem} />
         ))}
       </List>
     </Paper>
@@ -39,7 +37,7 @@ function App() {
 
   return (<div className="App">
       <Container maxWidth="md">
-        <AddTodo />
+        <AddTodo addItem={addItem} />
         <div className="TodoList">{todoItems}</div>
       </Container>
     </div>);
--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,15 @@
 import React, {useState} from "react";
-import {ListItem, ListItemText, InputBase, Checkbox} from "@mui/material";
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@mui/material";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
 
 const Todo = (props) => {
-    const [item, setItem] = useState(props.item);
+    const [item, setItem] = useState([props.item]);
+    const deleteItem = props.deleteItem;
+
+    // deleteEventHandler
+    const deleteEventHandler = () => {
+        deleteItem(item);
+    };
 
     return (
         <ListItem>
@@ -11,6 +18,11 @@ const Todo = (props) => {
                 <InputBase inputProps={{ "aria-label": "naked" }} type="text" id={item.id} name={item.id} value={item.title}
                 multiline={true} fullWidth={true}/>
             </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Delete Todo" onClick={deleteEventHandler}>
+                    <DeleteOutlined />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     );
 };
