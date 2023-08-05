179249b17301b7c992292cbab51f5cef3df0ac7c
minhuikim <kmh940@gmail.com>
2023-08-05 14:08:07 +0900

    20230805 UI글리치 해결

--- src/App.js
+++ src/App.js
@@ -17,10 +17,15 @@ import { call, signout } from "./service/ApiService";
 function App() {
   // deleteItem 추가 후 테스트를 위해 작성한 useState([...]) 배열을 빈 배열로 초기화
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null)
-    .then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => {
+      setItems(response.data);
+      // 20230805 UI 글리치 해결
+      // (로그인하지 않은 상태로 localhost:3000에 접속할 경우 로그인 페이지로 라우팅하기까지 걸리는 시간동안 todo리스트 페이지가 보이다가 로그인화면으로 넘어가는 현상)
+      setLoading(false);
+    });
   }, [])
 
   // 새로운 Todo 추가
@@ -69,15 +74,31 @@ function App() {
     </AppBar>
   );
 
-  return (
-    <div className="App">
+  // 20230805 UI 글리치 해결
+  //로딩중이 아닐 때 렌더링할 부분
+  let todoListPage = (
+    <div>
       {navigationBar} {/* 네비게이션 바 렌더링 */}
       <Container maxWidth="md">
         <AddTodo addItem={addItem} />
-        <div className="TodoList">{todoItems}</div>
+        <div className="TodoList">
+          {todoItems}
+        </div>
       </Container>
     </div>
   );
+
+  // 로딩중 일 때 렌더링 할 부분
+  let loadingPage = <h1> 로딩중.. </h1>
+  let content = loadingPage;
+
+  if (!loading) {
+    // 로딩중이 아니면 todoListPage를 선택
+    content = todoListPage;
+  }
+
+  // 선택한 content 렌더링
+  return <div className="App">{content}</div>;
 }
 
 export default App;
