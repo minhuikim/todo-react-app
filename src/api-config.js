1f6964f575f4b7cb307d80e1892df4609709be3d
minhuikim <kmh940@gmail.com>
2023-08-30 23:40:20 +0900

    20230830 AWS url, 프론트엔드 통합

--- src/api-config.js
+++ src/api-config.js
@@ -1,12 +1,14 @@
-/**
- * 백엔드 서비스 주소인 http://localhost:8080을 변수에 담고 현재 브라우저의 도메인이 localhost인 경우 로컬 호스트에서 동작하는 백엔드 애플리케이션을 사용
- */
 let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname == "localhost") {
-    backendHost = "http://localhost:8080";
+/**
+ * 백엔드 서비스 주소인 http://localhost:8080을 변수에 담고 현재 브라우저의 도메인이 localhost인 경우 로컬 호스트에서 동작하는 백엔드 애플리케이션을 사용
+ * 
+ * 20230830 Back-end AWS 빌드 후 프론트엔드 통합 작업 및 테스팅
+ */
+if (hostname === "localhost") {
+    backendHost = "http://prod-todo-api-service-m.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
